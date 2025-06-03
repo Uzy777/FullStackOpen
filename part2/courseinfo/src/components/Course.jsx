a5047a65c85abe48b424e0104ab5193490b731cc
Uzy777 <usmon.h@outlook.com>
2025-06-03 22:38:09 +0100

    Part 2a Rendering a collection, modules - Completed: 2.5 Separate module - step 10

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,31 @@
+const Header = (props) => <h1>{props.course}</h1>;
+
+const Content = (props) => (
+  <div>
+    {props.parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+);
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+);
+
+const Total = (props) => {
+  return <strong>Total of {props.parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</strong>;
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default Course;
