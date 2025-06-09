b57fa8bc050b4136473496ffc45f58f5d35a5dc1
Uzy777 <usmon.h@outlook.com>
2025-06-09 21:08:30 +0100

    Part 2b Forms - Contents

--- temp/src/App.jsx
+++ temp/src/App.jsx
@@ -1,32 +1,16 @@
-import { useState } from "react";
+import Note from './components/Note'
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return <div>the app is used by pressing the buttons</div>;
-  }
-  return <div>button press history: {props.allClicks.join(" ")}</div>;
-};
-
-const Display = (props) => <div>{props.value}</div>;
-
-const Button = (props) => <button onClick={props.onClick}>{props.text}</button>;
-
-const App = () => {
-  const [value, setValue] = useState(10);
-
-  const setToValue = (newValue) => {
-    console.log("value now", newValue);
-    setValue(newValue);
-  };
-
+const App = ({ notes }) => {
   return (
     <div>
-      <Display value={value} />
-      <Button onClick={() => setToValue(1000)} text="thousand" />
-      <Button onClick={() => setToValue(0)} text="reset" />
-      <Button onClick={() => setToValue(value + 1)} text="increment" />
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
