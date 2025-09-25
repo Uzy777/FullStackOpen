3348657dde6f6bbc68505398fc53f1c44171daac
Uzy777 <usmon.h@outlook.com>
2025-09-25 23:06:17 +0100

    Part 2e Adding styles to React app - Completed: 2.18* Data for countries - step 1

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -2,29 +2,72 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const App = () => {
+    const [value, setValue] = useState("");
+    const [allCountries, setAllCountries] = useState([]);
     const [country, setCountry] = useState(null);
+    const [updateMessage, setUpdateMessage] = useState("");
 
     useEffect(() => {
-        console.log("run this now");
+        axios.get("https://studies.cs.helsinki.fi/restcountries/api/all").then((response) => setAllCountries(response.data));
+    }, []);
 
-        if (country) {
-            console.log("Country found fetch results...");
-            axios.get(`https://studies.cs.helsinki.fi/restcountries/api/name/${country}`).then((response) => {
-                console.log("found");
-            });
-        }
+    //     axios
+    //         .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${country}`)
+    //         .then((response) => {
+    //             console.log(response.data);
+    //             console.log("found");
+    //         })
+    //         .catch((error) => {
+    //             console.log("no matches found");
+    //             setUpdateMessage("Too many matches, specify another filter")
+    //         });
+    // }, [country]);
 
-        else {
-          console.log("not found")
-        }
-    }, [country]);
+    const filtered = allCountries.filter((country) => country.name.common.toLowerCase().includes(value.toLowerCase()));
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    };
+
+    const onSearch = (event) => {
+        event.preventDefault();
+        setCountry(value);
+    };
 
     return (
-        <>
-            <label>Find Countries: </label>
+        <div>
+            <form onSubmit={onSearch}>
+                Find Countries: <input value={value} onChange={handleChange} />
+                {/* <button type="submit">Submit</button> */}
+                {filtered.length > 10 && <p>Too many matches, specify antoher filter</p>}
+                {filtered.length > 0 && filtered.length <= 10 && (
+                    <ul>
+                        {filtered.map((country) => (
+                            <li key={country.cca3}>{country.name.common}</li>
+                        ))}
+                    </ul>
+                )}{" "}
+                {filtered.length === 1 && (
+                    <div>
+                        <h1>{filtered[0].name.common}</h1>
+                        <p>Capital: {filtered[0].capital}</p>
+                        <p>Area: {filtered[0].area}</p>
+                        <br />
+                        <h2>Languages</h2>
+                        <ul>
+                            {Object.values(filtered[0].languages).map((lang) => (
+                                <li key={lang}>{lang}</li>
+                            ))}
+                        </ul>
+                        <img src={filtered[0].flags.png} alt={filtered[0].flags.alt} />
+                    </div>
+                )}
+            </form>
+
+            {/* <label>Find Countries: </label>
             <input type="text" />
-            <p>Too many matches, specify antoher filter</p>
-        </>
+            <p>Too many matches, specify antoher filter</p> */}
+        </div>
     );
 };
 
