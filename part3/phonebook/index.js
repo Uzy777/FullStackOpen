d036bc69855e6655271a59d994f0341f58358a09
Uzy777 <usmon.h@outlook.com>
2025-10-08 19:02:52 +0100

    Part 3a Node.js and Express - Completed: 3.7 Phonebook backend - step 7

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,8 +1,10 @@
-// http://localhost:3001/api/persons
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
 
 app.use(express.json());
+app.use(morgan("tiny"));
 
 let persons = [
     {
