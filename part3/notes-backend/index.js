ef920945b55dc5498cb0a740d611405b70a50a8e
Uzy777 <usmon.h@outlook.com>
2025-10-20 19:39:06 +0100

    Part 3c Saving data to MongoDB - Defining environment variables using the dotenv library - Completed

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,9 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const Note = require("./models/note");
 
 const app = express();
-
-const Note = require("./models/note");
 
 app.get("/api/notes", (request, response) => {
     Note.find({}).then((notes) => {
