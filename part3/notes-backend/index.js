6cf804f143d73aa721e5238db30a99b40f288213
Uzy777 <usmon.h@outlook.com>
2025-10-20 19:58:51 +0100

    Part 3c Saving data to MongoDB - Using database in route handlers - Completed

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -50,40 +50,26 @@ app.get("/api/notes", (request, response) => {
     response.json(notes);
 });
 
-app.get("/api/notes/:id", (request, response) => {
-    const id = request.params.id;
-    const note = notes.find((note) => note.id === id);
+// app.get("/api/notes/:id", (request, response) => {
+//     const id = request.params.id;
+//     const note = notes.find((note) => note.id === id);
 
-    if (note) {
-        response.json(note);
-    } else {
-        response.status(404).end();
-    }
-});
+//     if (note) {
+//         response.json(note);
+//     } else {
+//         response.status(404).end();
+//     }
+// });
 
 const generateId = () => {
     const maxId = notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
     return String(maxId + 1);
 };
 
-app.post("/api/notes", (request, response) => {
-    const body = request.body;
-
-    if (!body.content) {
-        return response.status(400).json({
-            error: "content missing",
-        });
-    }
-
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        id: generateId(),
-    };
-
-    notes = notes.concat(note);
-
-    response.json(note);
+app.get("/api/notes/:id", (request, response) => {
+    Note.findById(request.params.id).then((note) => {
+        response.json(note);
+    });
 });
 
 app.delete("/api/notes/:id", (request, response) => {
