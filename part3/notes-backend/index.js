a61e9ebc0dffe4dc48f23bd99eead166826d23f5
Uzy777 <usmon.h@outlook.com>
2025-10-21 19:13:55 +0100

    Part 3c Saving data to MongoDB - Moving error handling into middleware - Completed

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -28,7 +28,7 @@ app.get("/api/notes", (request, response) => {
     });
 });
 
-app.get("/api/notes/:id", (request, response) => {
+app.get("/api/notes/:id", (request, response, next) => {
     Note.findById(request.params.id)
         .then((note) => {
             if (note) {
@@ -37,10 +37,7 @@ app.get("/api/notes/:id", (request, response) => {
                 response.status(404).end();
             }
         })
-        .catch((error) => {
-            console.log(error);
-            response.status(400).send({ error: "malformatted id" });
-        });
+        .catch((error) => next(error));
 });
 
 app.post("/api/notes", (request, response) => {
@@ -72,6 +69,19 @@ const unknownEndpoint = (request, response) => {
 };
 
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if (error.name === "CastError") {
+        return response.status(400).send({ error: "malformatted id" });
+    }
+
+    next(error);
+};
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
