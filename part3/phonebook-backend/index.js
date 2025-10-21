071b6cfd836875b7aa90b4f0a3bf9e705c0f441c
Uzy777 <usmon.h@outlook.com>
2025-10-21 20:43:57 +0100

    Part 3c Saving data to MongoDB - Completed: 3.17* Phonebook database - step 5

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -159,6 +159,28 @@ app.post("/api/persons", (request, response, next) => {
 //     response.json(person);
 // });
 
+// PUT REQUESTS
+
+// Change number for existing person name
+app.put("/api/persons/:id", (request, response, next) => {
+    const { name, number } = request.body;
+
+    Person.findById(request.params.id)
+        .then((person) => {
+            if (!person) {
+                return response.status(404).end();
+            }
+
+            person.name = name;
+            person.number = number;
+
+            return person.save().then((updatedPerson) => {
+                response.json(updatedPerson);
+            });
+        })
+        .catch((error) => next(error));
+});
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
