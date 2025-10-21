7a990f4ec6652872a5ac78f1c8b1cd931d8f8714
Uzy777 <usmon.h@outlook.com>
2025-10-21 20:35:36 +0100

    Part 3c Saving data to MongoDB - Completed: 3.16 Phonebook database - step 4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -60,10 +60,12 @@ app.get("/info", (request, response) => {
 });
 
 // Get for all persons
-app.get("/api/persons", (request, response) => {
-    Person.find({}).then((persons) => {
-        response.json(persons);
-    });
+app.get("/api/persons", (request, response, next) => {
+    Person.find({})
+        .then((persons) => {
+            response.json(persons);
+        })
+        .catch((error) => next(error));
 });
 
 // app.get("/api/persons", (request, response) => {
@@ -91,15 +93,12 @@ app.get("/api/persons/:id", (request, response) => {
 // DELETE REQUESTS //
 
 // Delete a single person ID entry
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
     Person.findByIdAndDelete(request.params.id)
         .then((result) => {
             response.status(204).end();
         })
-        .catch((error) => {
-            console.log(error);
-            response.status(400).json({ error: "malformatted id" });
-        });
+        .catch((error) => next(error));
 });
 
 // app.delete("/api/persons/:id", (request, response) => {
@@ -112,7 +111,7 @@ app.delete("/api/persons/:id", (request, response) => {
 // POST REQUESTS //
 
 // Post new phonebook entry
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
     const body = request.body;
 
     if (!body.name) {
@@ -124,9 +123,12 @@ app.post("/api/persons", (request, response) => {
         number: body.number,
     });
 
-    person.save().then((savedPerson) => {
-        response.json(savedPerson);
-    });
+    person
+        .save()
+        .then((savedPerson) => {
+            response.json(savedPerson);
+        })
+        .catch((error) => next(error));
 });
 
 // const generateId = () => {
