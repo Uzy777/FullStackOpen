eace908f5ccbd789eda7191ec54e847a37a3435e
Uzy777 <usmon.h@outlook.com>
2025-10-22 20:45:41 +0100

    Part 3d Validation and ESLint - Completed: 3.20* Phonebook database - step 8

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -21,7 +21,14 @@ const personSchema = new mongoose.Schema({
         minLength: 3,
         required: true,
     },
-    number: String,
+    number: {
+        type: String,
+        validate: {
+            validator: function (v) {
+                return /^\d{2,3}-\d{7,8}$/.test(v);
+            },
+        },
+    },
 });
 
 personSchema.set("toJSON", {
