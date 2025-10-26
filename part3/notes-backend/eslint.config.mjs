924eff4e4ea9cfab884faca5c2ace44ae251a1ad
Uzy777 <usmon.h@outlook.com>
2025-10-26 19:26:58 +0000

    Part 3d Validation and ESLint - Formatting the Configuration File - Completed

--- part3/notes-backend/eslint.config.mjs
+++ part3/notes-backend/eslint.config.mjs
@@ -1,8 +1,22 @@
-import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import stylisticJs from "@stylistic/eslint-plugin-js";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-]);
+export default [
+    js.configs.recommended,
+    {
+        files: ["**/*.js"],
+        languageOptions: {
+            sourceType: "commonjs",
+            globals: { ...globals.node },
+            ecmaVersion: "latest",
+        },
+        plugins: { "@stylistic/js": stylisticJs },
+        rules: {
+            "@stylistic/js/indent": ["error", 2],
+            "@stylistic/js/linebreak-style": ["error", "unix"],
+            "@stylistic/js/quotes": ["error", "single"],
+            "@stylistic/js/semi": ["error", "never"],
+        },
+    },
+];
