646c6ab38936ec36b1bfef8aa0b30cae044c9c4b
Uzy777 <usmon.h@outlook.com>
2025-11-04 21:24:13 +0000

    Part 4a Structure of backend application, introduction to testing - Completed: 4.2 Blog List - step 2

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
 
+const app = require("./app");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
+
 const express = require("express");
 const mongoose = require("mongoose");
 
-const app = express();
+// const app = express();
 
 const blogSchema = mongoose.Schema({
     title: String,
@@ -12,28 +16,18 @@ const blogSchema = mongoose.Schema({
     likes: Number,
 });
 
-const Blog = mongoose.model("Blog", blogSchema);
+// const Blog = mongoose.model("Blog", blogSchema);
 
 // const password = process.argv[2];
 
 const mongoUrl = process.env.MONGODB_URI;
 mongoose.connect(mongoUrl);
 
+app.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`);
+});
+
 app.use(express.json());
-
-app.get("/api/blogs", (request, response) => {
-    Blog.find({}).then((blogs) => {
-        response.json(blogs);
-    });
-});
-
-app.post("/api/blogs", (request, response) => {
-    const blog = new Blog(request.body);
-
-    blog.save().then((result) => {
-        response.status(201).json(result);
-    });
-});
 
 const PORT = 3003;
 app.listen(PORT, () => {
