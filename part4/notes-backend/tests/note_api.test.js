da4a14299758dd60927b1cb667c8cd830ee48efe
Uzy777 <usmon.h@outlook.com>
2025-11-10 19:40:07 +0000

    Part 4b Testing the backend - Running tests one by one - Completed

--- part4/notes-backend/tests/note_api.test.js
+++ part4/notes-backend/tests/note_api.test.js
@@ -46,6 +46,19 @@ test("a specific note is within the returned notes", async () => {
     assert(contents.includes("HTML is easy"));
 });
 
+test.only("notes are returned as json", async () => {
+    await api
+        .get("/api/notes")
+        .expect(200)
+        .expect("Content-Type", /application\/json/);
+});
+
+test.only("all notes are returned", async () => {
+    const response = await api.get("/api/notes");
+
+    assert.strictEqual(response.body.length, 2);
+});
+
 after(async () => {
     await mongoose.connection.close();
 });
