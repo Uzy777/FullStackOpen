156a50d8159c874f63834e3cd6cf5790a90d0f7c
Uzy777 <usmon.h@outlook.com>
2025-11-25 21:15:22 +0000

    Part 4d Token authentication - Completed: 4.15 Blog List Expansions - step 3

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+
+// Utils
 const config = require("./utils/config");
 const logger = require("./utils/logger");
-const blogsRouter = require("./controllers/blogs");
 const middleware = require("./utils/middleware");
+
+// Controllers
+const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
 
 const app = express();
 
@@ -23,6 +28,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
