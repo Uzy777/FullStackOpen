027c339f5b8103ed8166494a6fe524f00e2210d7
Uzy777 <usmon.h@outlook.com>
2025-11-27 14:26:56 +0000

    Part 5a Login in frontend - Adding a Login Form - Completed

--- part5/notes-frontend/src/App.jsx
+++ part5/notes-frontend/src/App.jsx
@@ -9,6 +9,8 @@ const App = () => {
     const [newNote, setNewNote] = useState("");
     const [showAll, setShowAll] = useState(true);
     const [errorMessage, setErrorMessage] = useState(null);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
     useEffect(() => {
         noteService.getAll().then((initialNotes) => {
@@ -53,10 +55,33 @@ const App = () => {
 
     const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
+    const handleLogin = (event) => {
+        event.preventDefault();
+        console.log("logging in with", username, password);
+    };
+
     return (
         <div>
             <h1>Notes</h1>
             <Notification message={errorMessage} />
+
+            <h2>Login</h2>
+            <form onSubmit={handleLogin}>
+                <div>
+                    <label>
+                        username
+                        <input type="text" value={username} onChange={({ target }) => setUsername(target.value)} />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        password
+                        <input type="password" value={password} onChange={({ target }) => setPassword(target.value)} />
+                    </label>
+                </div>
+                <button type="submit">login</button>
+            </form>
+
             <div>
                 <button onClick={() => setShowAll(!showAll)}>show {showAll ? "important" : "all"}</button>
             </div>
