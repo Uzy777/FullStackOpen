3951dc8d29191641597d66829b194c20884d84f3
Uzy777 <usmon.h@outlook.com>
2025-11-28 10:42:04 +0000

    Part 5a Login in frontend - Completed: 5.1 Blog List Frontend - step 1

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,24 +1,64 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+    const [blogs, setBlogs] = useState([]);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    useEffect(() => {
+        blogService.getAll().then((blogs) => setBlogs(blogs));
+    }, []);
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
-}
+    const handleLogin = async (event) => {
+        event.preventDefault();
+        try {
+            const user = await loginService.login({ username, password });
+            setUser(user);
+            setUsername("");
+            setPassword("");
+        } catch {
+            console.log("wrong credentials");
+            setTimeout(() => {
+                console.log(null);
+            }, 5000);
+        }
+    };
 
-export default App
+    if (user === null) {
+        return (
+            <div>
+                <h2>Log in to application</h2>
+                <form onSubmit={handleLogin}>
+                    <div>
+                        <label>
+                            username
+                            <input type="text" value={username} onChange={({ target }) => setUsername(target.value)} />
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            password
+                            <input type="password" value={password} onChange={({ target }) => setPassword(target.value)} />
+                        </label>
+                    </div>
+                    <button type="submit">login</button>
+                </form>
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            <h2>blogs</h2>
+            {blogs.map((blog) => (
+                <Blog key={blog.id} blog={blog} />
+            ))}
+        </div>
+    );
+};
+
+export default App;
