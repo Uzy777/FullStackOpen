62a0f8ebc5587ce7bb34138cc0e23ce4cf680e71
Uzy777 <usmon.h@outlook.com>
2025-12-08 16:38:36 +0000

    Part 5b props.children and proptypes - Completed: 5.7 Blog List Frontend - step 7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,39 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+    // Step 1: local state to track visibility
+    const [visible, setVisible] = useState(false);
+
+    // Step 2: function to toggle visibility
+    const toggleVisibility = () => setVisible(!visible);
+
+    // Step 3: basic inline style
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: "solid",
+        borderWidth: 1,
+        marginBottom: 5,
+    };
+
+    return (
+        <div style={blogStyle}>
+            <div>
+                {blog.title} {blog.author}
+                <button onClick={toggleVisibility}>{visible ? "hide" : "view"}</button>
+            </div>
+
+            {visible && (
+                <div>
+                    <div>{blog.url}</div>
+                    <div>
+                        likes {blog.likes} <button>like</button>
+                    </div>
+                    <div>{blog.author}</div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Blog;
