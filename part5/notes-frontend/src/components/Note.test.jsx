a1ef59afb4ec3e0b5e3bf5f1c5d8e4bec95373ec
Uzy777 <usmon.h@outlook.com>
2025-12-09 11:18:42 +0000

    Part 5c Testing React apps - Clicking buttons in tests - Completed

--- part5/notes-frontend/src/components/Note.test.jsx
+++ part5/notes-frontend/src/components/Note.test.jsx
@@ -1,17 +1,19 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Note from "./Note";
 
-test("renders content", () => {
+test("clicking the button calls event handler once", async () => {
     const note = {
         content: "Component testing is done with react-testing-library",
         important: true,
     };
 
-    render(<Note note={note} />);
+    const mockHandler = vi.fn();
+    render(<Note note={note} toggleImportance={mockHandler} />);
 
-    const element = screen.getByText("Component testing is done with react-testing-library");
+    const user = userEvent.setup();
+    const button = screen.getByText("make not important");
+    await user.click(button);
 
-    screen.debug(element);
-
-    expect(element).toBeDefined();
+    expect(mockHandler.mock.calls).toHaveLength(1);
 });
