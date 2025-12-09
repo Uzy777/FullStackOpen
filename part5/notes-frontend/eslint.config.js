a884e3a5c55669f550afcfc0af5f42255f7cfaee
Uzy777 <usmon.h@outlook.com>
2025-12-09 11:02:45 +0000

    Part 5c Testing React apps - Rendering the component for tests - Completed

--- part5/notes-frontend/eslint.config.js
+++ part5/notes-frontend/eslint.config.js
@@ -37,4 +37,12 @@ export default [
             "no-console": "off",
         },
     },
+    {
+        files: ["**/*.test.{js,jsx}"],
+        languageOptions: {
+            globals: {
+                ...globals.vitest,
+            },
+        },
+    },
 ];
