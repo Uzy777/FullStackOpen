c3c76faee9742a20b41acc2a7298b3a2efd78100
Uzy777 <usmon.h@outlook.com>
2025-12-09 14:51:30 +0000

    Part 5d End to end testing: Playwright - Testing note creation - Completed

--- tests/note_app.spec.js
+++ tests/note_app.spec.js
@@ -14,14 +14,26 @@ describe("Note app", () => {
     });
 
     test("user can log in", async ({ page }) => {
-        await page.goto("http://localhost:5173");
-
         await page.getByRole("button", { name: "login" }).click();
         await page.getByLabel("username").fill("mluukkai");
         await page.getByLabel("password").fill("salainen");
-
         await page.getByRole("button", { name: "login" }).click();
-
         await expect(page.getByText("Matti Luukkainen logged in")).toBeVisible();
+    });
+
+    describe("when logged in", () => {
+        beforeEach(async ({ page }) => {
+            await page.getByRole("button", { name: "login" }).click();
+            await page.getByLabel("username").fill("mluukkai");
+            await page.getByLabel("password").fill("salainen");
+            await page.getByRole("button", { name: "login" }).click();
+        });
+
+        test("a new note can be created", async ({ page }) => {
+            await page.getByRole("button", { name: "new note" }).click();
+            await page.getByRole("textbox").fill("a note created by playwright");
+            await page.getByRole("button", { name: "save" }).click();
+            await expect(page.getByText("a note created by playwright")).toBeVisible();
+        });
     });
 });
