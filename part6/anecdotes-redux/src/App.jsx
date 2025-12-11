295a3b6c3b7755ec2e9d614ecac5e86b0c875064
Uzy777 <usmon.h@outlook.com>
2025-12-11 19:35:34 +0000

    Part 6a Flux-architecture and Redux - Completed: 6.7 Anecdotes - step 5

--- part6/anecdotes-redux/src/App.jsx
+++ part6/anecdotes-redux/src/App.jsx
@@ -1,6 +1,5 @@
 import { useSelector, useDispatch } from "react-redux";
-
-const generateId = () => Number((Math.random() * 1000000).toFixed(0));
+import AnecdoteForm from "./components/AnecdoteForm";
 
 const App = () => {
     const dispatch = useDispatch();
@@ -11,20 +10,6 @@ const App = () => {
         dispatch({
             type: "VOTE",
             payload: id,
-        });
-    };
-
-    const addAnecdote = (event) => {
-        event.preventDefault();
-        const content = event.target.anecdote.value;
-        event.target.anecdote.value = "";
-        dispatch({
-            type: "NEW_ANECDOTE",
-            payload: {
-                content,
-                id: generateId(),
-                votes: 0,
-            },
         });
     };
 
@@ -41,13 +26,7 @@ const App = () => {
                 </div>
             ))}
 
-            <h2>create new</h2>
-            <form onSubmit={addAnecdote}>
-                <div>
-                    <input name="anecdote" />
-                </div>
-                <button type="submit">create</button>
-            </form>
+            <AnecdoteForm />
         </div>
     );
 };
