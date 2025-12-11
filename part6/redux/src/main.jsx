1556dc8ccff27506dee26253c370e6827732c5b7
Uzy777 <usmon.h@outlook.com>
2025-12-11 13:47:36 +0000

    Part 6a Flux-architecture and Redux - Pure functions, immutable - Completed

--- part6/redux/src/main.jsx
+++ part6/redux/src/main.jsx
@@ -4,8 +4,7 @@ import { createStore } from "redux";
 const noteReducer = (state = [], action) => {
     switch (action.type) {
         case "NEW_NOTE":
-            state.push(action.payload);
-            return state;
+            return state.concat(action.payload);
         default:
             return state;
     }
@@ -27,6 +26,13 @@ store.dispatch({
     payload: {
         content: "state changes are made with actions",
         important: false,
+        id: 2,
+    },
+});
+
+store.dispatch({
+    type: "TOGGLE_IMPORTANCE",
+    payload: {
         id: 2,
     },
 });
