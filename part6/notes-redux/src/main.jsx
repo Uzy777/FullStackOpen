83cc46121919e8a7664aace0468fb223aa86b788
Uzy777 <usmon.h@outlook.com>
2025-12-24 00:33:43 +0000

    Part 6b Many reducers - Combined reducers - Completed

--- part6/notes-redux/src/main.jsx
+++ part6/notes-redux/src/main.jsx
@@ -1,14 +1,29 @@
 import ReactDOM from "react-dom/client";
-import { createStore } from "redux";
 import { Provider } from "react-redux";
+import { createStore, combineReducers } from "redux";
 
 import App from "./App";
+import filterReducer from "./reducers/filterReducer";
 import noteReducer from "./reducers/noteReducer";
 
-const store = createStore(noteReducer);
+const reducer = combineReducers({
+    notes: noteReducer,
+    filter: filterReducer,
+});
+
+const store = createStore(reducer);
+
+console.log(store.getState());
+
+import { createNote } from "./reducers/noteReducer";
+import { filterChange } from "./reducers/filterReducer";
+
+store.subscribe(() => console.log(store.getState()));
+store.dispatch(filterChange("IMPORTANT"));
+store.dispatch(createNote("combineReducers forms one reducer from many simple reducers"));
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <Provider store={store}>
-        <App />
+        <div />
     </Provider>
 );
