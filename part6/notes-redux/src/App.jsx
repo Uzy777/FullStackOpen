2fd9bb5cf72fc273c613395cb40199593126ae52
Uzy777 <usmon.h@outlook.com>
2025-12-24 00:28:53 +0000

    Part 6b Many reducers - Store with complex state - Completed

--- part6/notes-redux/src/App.jsx
+++ part6/notes-redux/src/App.jsx
@@ -2,9 +2,21 @@ import NoteForm from "./components/NoteForm";
 import Notes from "./components/Notes";
 
 const App = () => {
+    const filterSelected = (value) => {
+        console.log(value);
+    };
+
     return (
         <div>
             <NoteForm />
+            <div>
+                <input type="radio" name="filter" onChange={() => filterSelected("ALL")} />
+                all
+                <input type="radio" name="filter" onChange={() => filterSelected("IMPORTANT")} />
+                important
+                <input type="radio" name="filter" onChange={() => filterSelected("NONIMPORTANT")} />
+                nonimportant
+            </div>
             <Notes />
         </div>
     );
