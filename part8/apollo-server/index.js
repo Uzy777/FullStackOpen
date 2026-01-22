4153520d907012472a5d59da97b379b570c3e6b7
Uzy777 <usmon.h@outlook.com>
2026-01-22 20:57:26 +0000

    Part 8a GraphQL-server - Error handling - Completed

--- part8/apollo-server/index.js
+++ part8/apollo-server/index.js
@@ -1,6 +1,7 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { v1 as uuid } from "uuid";
+import { GraphQLError } from "graphql";
 
 let persons = [
     {
@@ -67,6 +68,15 @@ const resolvers = {
     },
     Mutation: {
         addPerson: (root, args) => {
+            if (persons.find((p) => p.name === args.name)) {
+                throw new GraphQLError(`Name must be unique: ${args.name}`, {
+                    extensions: {
+                        code: "BAD_USER_INPUT",
+                        invlaidArgs: args.name,
+                    },
+                });
+            }
+
             const person = { ...args, id: uuid() };
             persons = persons.concat(person);
             return person;
