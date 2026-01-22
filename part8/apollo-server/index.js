4544e3572ae34ca02e533a5b5a9d70ed500c9fe1
Uzy777 <usmon.h@outlook.com>
2026-01-22 20:46:44 +0000

    Part 8a GraphQL-server - Object within an object - Completed

--- part8/apollo-server/index.js
+++ part8/apollo-server/index.js
@@ -25,11 +25,16 @@ let persons = [
 ];
 
 const typeDefs = /* GraphQL */ `
+    type Address {
+        street: String!
+        city: String!
+    }
+
     type Person {
         name: String!
         phone: String
-        street: String!
-        city: String!
+
+        address: Address!
         id: ID!
     }
 
@@ -46,12 +51,14 @@ const resolvers = {
         allPersons: () => persons,
         findPerson: (root, args) => persons.find((p) => p.name === args.name),
     },
+
     Person: {
-        name: (root) => root.name,
-        phone: (root) => root.phone,
-        street: (root) => root.street,
-        city: (root) => root.city,
-        id: (root) => root.id,
+        address: ({ street, city }) => {
+            return {
+                street,
+                city,
+            };
+        },
     },
 };
 
