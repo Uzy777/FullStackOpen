b6fb837d5e51d979e9e7ae5308b2ccfd60f56e42
Uzy777 <usmon.h@outlook.com>
2026-01-28 19:22:52 +0000

    Part 8c Database and user administration - User and log in - Completed

--- part8/graphql-phonebook-backend/resolvers.js
+++ part8/graphql-phonebook-backend/resolvers.js
@@ -1,5 +1,7 @@
 const { GraphQLError } = require("graphql");
 const Person = require("./models/person");
+const User = require("./models/user");
+const jwt = require("jsonwebtoken");
 
 const resolvers = {
     Query: {
@@ -72,6 +74,37 @@ const resolvers = {
             }
 
             return person;
+        },
+        createUser: async (root, args) => {
+            const user = new User({ username: args.username });
+
+            return user.save().catch((error) => {
+                throw new GraphQLError(`Creating the user failed: ${error.message}`, {
+                    extensions: {
+                        code: "BAD_USER_INPUT",
+                        invalidArgs: args.username,
+                        error,
+                    },
+                });
+            });
+        },
+        login: async (root, args) => {
+            const user = await User.findOne({ username: args.username });
+
+            if (!user || args.password !== "secret") {
+                throw new GraphQLError("wrong credentials", {
+                    extensions: {
+                        code: "BAD_USER_INPUT",
+                    },
+                });
+            }
+
+            const userForToken = {
+                username: user.username,
+                id: user._id,
+            };
+
+            return { value: jwt.sign(userForToken, process.env.JWT_SECRET) };
         },
     },
 };
