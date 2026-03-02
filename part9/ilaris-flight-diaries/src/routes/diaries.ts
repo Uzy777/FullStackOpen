dd73baaf228a445b9e2b91ae3fdbdb79dd3462d8
Uzy777 <usmon.h@outlook.com>
2026-03-02 21:56:18 +0000

    Part 9c Typing an Express app - Validating requests - Completed

--- part9/ilaris-flight-diaries/src/routes/diaries.ts
+++ part9/ilaris-flight-diaries/src/routes/diaries.ts
@@ -2,6 +2,8 @@ import express from "express";
 
 import diaryService from "../services/diaryService";
 
+import toNewDiaryEntry from "../utils";
+
 const router = express.Router();
 
 router.get("/", (_req, res) => {
@@ -23,15 +25,17 @@ router.get("/:id", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-    const { date, weather, visibility, comment } = req.body;
-
-    const addedEntry = diaryService.addDiary({
-        date,
-        weather,
-        visibility,
-        comment,
-    });
-    res.json(addedEntry);
+    try {
+        const newDiaryEntry = toNewDiaryEntry(req.body);
+        const addedEntry = diaryService.addDiary(newDiaryEntry);
+        res.json(addedEntry);
+    } catch (error: unknown) {
+        let errorMessage = "Something went wrong.";
+        if (error instanceof Error) {
+            errorMessage += " Error: " + error.message;
+        }
+        res.status(400).send(errorMessage);
+    }
 });
 
 export default router;
