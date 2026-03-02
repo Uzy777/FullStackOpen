97375b9dd051a32e478afd90d5f6015493164543
Uzy777 <usmon.h@outlook.com>
2026-03-02 21:49:44 +0000

    Part 9c Typing an Express app - Adding a new diary - Completed

--- part9/ilaris-flight-diaries/src/routes/diaries.ts
+++ part9/ilaris-flight-diaries/src/routes/diaries.ts
@@ -22,4 +22,16 @@ router.get("/:id", (req, res) => {
     }
 });
 
+router.post("/", (req, res) => {
+    const { date, weather, visibility, comment } = req.body;
+
+    const addedEntry = diaryService.addDiary({
+        date,
+        weather,
+        visibility,
+        comment,
+    });
+    res.json(addedEntry);
+});
+
 export default router;
--- part9/ilaris-flight-diaries/src/services/diaryService.ts
+++ part9/ilaris-flight-diaries/src/services/diaryService.ts
@@ -1,6 +1,6 @@
 import diaries from "../../data/entries";
 
-import { NonSensitiveDiaryEntry, DiaryEntry } from "../types";
+import { NewDiaryEntry, NonSensitiveDiaryEntry, DiaryEntry } from "../types";
 
 const getEntries = (): DiaryEntry[] => {
     return diaries;
@@ -15,8 +15,14 @@ const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
     }));
 };
 
-const addDiary = () => {
-    return null;
+const addDiary = (entry: NewDiaryEntry): DiaryEntry => {
+    const newDiaryEntry = {
+        id: Math.max(...diaries.map((d) => d.id)) + 1,
+        ...entry,
+    };
+
+    diaries.push(newDiaryEntry);
+    return newDiaryEntry;
 };
 
 const findById = (id: number): DiaryEntry | undefined => {
