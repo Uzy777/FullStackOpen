75386c8f1b45e029ce687eeb251c2bfedb6aad24
Uzy777 <usmon.h@outlook.com>
2026-03-05 22:29:36 +0000

    Part 9c Typing an Express app - Completed: 9.14 Patientor backend - step 7

--- part9/patientor-backend/src/routes/patients.ts
+++ part9/patientor-backend/src/routes/patients.ts
@@ -4,18 +4,13 @@ import { toNewPatient } from "../utils";
 
 const router = express.Router();
 
-router.get("/", (_req, res) => {
-    const patients = patientsService.getNonSensitivePatients();
-    return res.json(patients);
-});
-
 router.post("/", (req, res) => {
     try {
         const newPatient = toNewPatient(req.body);
         const addedPatient = patientsService.addPatient(newPatient);
-        return res.json(addedPatient);
+        res.json(addedPatient);
     } catch (error) {
-        return res.status(400).send(error instanceof Error ? error.message : "Invalid data");
+        res.status(400).send(error instanceof Error ? error.message : "Invalid data");
     }
 });
 
