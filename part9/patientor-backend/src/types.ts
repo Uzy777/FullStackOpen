f5dca8734347b2f77c5637c66f34d011e0f3cd9e
Uzy777 <usmon.h@outlook.com>
2026-03-24 19:53:37 +0000

    Part 9e Grande finale: Patientor - Full entries - Completed

--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -1,31 +1,64 @@
-export interface Diagnosis {
-    id: string;
-    name: string;
-    dateOfBirth: string;
-    ssn: string;
-    gender: string;
-    occupation: string;
-    latin?: string;
-}
-
 export enum Gender {
     Male = "male",
     Female = "female",
     Other = "other",
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface Entry {}
+export interface Diagnosis {
+    code: string;
+    name: string;
+    latin?: string;
+}
+
+export enum HealthCheckRating {
+    Healthy = 0,
+    LowRisk = 1,
+    HighRisk = 2,
+    CriticalRisk = 3,
+}
+
+interface BaseEntry {
+    id: string;
+    description: string;
+    date: string;
+    specialist: string;
+    diagnosisCodes?: Array<Diagnosis["code"]>;
+}
+
+export interface HospitalEntry extends BaseEntry {
+    type: "Hospital";
+    discharge: {
+        date: string;
+        criteria: string;
+    };
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+    type: "OccupationalHealthcare";
+    employerName: string;
+    sickLeave?: {
+        startDate: string;
+        endDate: string;
+    };
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+    type: "HealthCheck";
+    healthCheckRating: HealthCheckRating;
+}
+
+export type Entry = HospitalEntry | OccupationalHealthcareEntry | HealthCheckEntry;
 
 export interface Patient {
     id: string;
     name: string;
-    dateOfBirth: string;
     ssn: string;
-    gender: Gender;
     occupation: string;
+    gender: Gender;
+    dateOfBirth: string;
     entries: Entry[];
 }
 
 export type NonSensitivePatient = Omit<Patient, "ssn" | "entries">;
 export type NewPatient = Omit<Patient, "id">;
+export type PatientFormValues = Omit<Patient, "id" | "entries">;
